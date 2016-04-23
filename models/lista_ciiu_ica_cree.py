57c1f72ef147da508dd0408470af25136f2d2bda
Dominic Krimmer <dominic.krimmer@gmail.com>
2016-04-23 18:54:23 -0500

    concatinate code and name for CIIU

--- models/lista_ciiu_ica_cree.py
+++ models/lista_ciiu_ica_cree.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 # CIIU, ICA y CREE
 
@@ -9,8 +9,16 @@ class CiiuIcaCree(models.Model):
     _name = "lista.ciiu_ica_cree"
     _description = "Lista CIIU, ICA y CREE"
 
-    codigo = fields.Char('Código', size=20, required=True)
-    name = fields.Char('Descripción', size=20, required=True)
-    parent = fields.Char('Padre', size=20, required=True)
+    codigo = fields.Char('Código', required=True)
+    name = fields.Char('Descripción')
+    parent = fields.Many2one('lista.ciiu_ica_cree', 'Padre')
     type = fields.Char('Tipo', size=20, required=True)
+    codigo_name = fields.Char(string="Codigo y Descripción", store=True, compute="_concat_name")
 
+    @api.one
+    @api.depends('codigo', 'name')
+    def _concat_name(self):
+        if self.codigo is False or self.name is False:
+            self.codigo_name = ''
+        else:
+            self.codigo_name = str(self.codigo) + ' - ' + str(self.name)
