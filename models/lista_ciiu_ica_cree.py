bcb0777ce679696adbe2a45eb323d24af5741a81
Dominic Krimmer <dominic.krimmer@gmail.com>
2016-04-23 23:06:27 -0500

    concatinate code and name for CIIU

--- models/lista_ciiu_ica_cree.py
+++ models/lista_ciiu_ica_cree.py
@@ -9,16 +9,21 @@ class CiiuIcaCree(models.Model):
     _name = "lista.ciiu_ica_cree"
     _description = "Lista CIIU, ICA y CREE"
 
+    name = fields.Char(string="Codigo y Descripción", store=True, compute="_concat_name")
     codigo = fields.Char('Código', required=True)
-    name = fields.Char('Descripción')
+    description = fields.Char('Descripción')
     parent = fields.Many2one('lista.ciiu_ica_cree', 'Padre')
     type = fields.Char('Tipo', size=20, required=True)
-    codigo_name = fields.Char(string="Codigo y Descripción", store=True, compute="_concat_name")
 
     @api.one
-    @api.depends('codigo', 'name')
+    @api.depends('codigo', 'description')
     def _concat_name(self):
-        if self.codigo is False or self.name is False:
-            self.codigo_name = ''
+        """
+        This function formats a new field in order to be able to search
+        for CIIU as number or string
+        @return: void
+        """
+        if self.codigo is False or self.description is False:
+            self.name = ''
         else:
-            self.codigo_name = str(self.codigo) + ' - ' + str(self.name)
+            self.name = str(self.codigo) + ' - ' + str(self.description)
