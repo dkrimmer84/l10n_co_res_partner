7a43f35b7ff6011e218e555575ce3bc89de4bc8c
Dominic Krimmer <dominic.krimmer@gmail.com>
2016-04-25 15:30:58 -0500

    added parent logic with some conditions Tree view: parents and child will be visually separated

--- models/ciiu.py
+++ models/ciiu.py
@@ -9,11 +9,20 @@ class Ciiu(models.Model):
     _name = "ciiu"
     _description = "Lista CIIU, ICA y CREE"
 
-    name = fields.Char(string="Codigo y Descripción", store=True, compute="_concat_name")
+    name = fields.Char(
+        string="Codigo y Descripción",
+        store=True,
+        compute="_concat_name"
+    )
     code = fields.Char('Código', required=True)
-    description = fields.Char('Descripción')
+    description = fields.Char('Descripción', required=True)
     parent = fields.Many2one('ciiu', 'Padre')
-    type = fields.Char('Tipo', size=20, required=True)
+    type = fields.Char(
+        'Tipo',
+        store=True,
+        compute="_set_type"
+    )
+    hasParent = fields.Boolean('Tiene Padre?')
 
     @api.one
     @api.depends('code', 'description')
@@ -26,4 +35,19 @@ class Ciiu(models.Model):
         if self.code is False or self.description is False:
             self.name = ''
         else:
-            self.name = str(self.code) + ' - ' + str(self.description)
+            self.name = str(self.code) + ' - ' + str(self.description.encode('utf-8').strip())
+
+    @api.one
+    @api.depends('hasParent')
+    def _set_type(self):
+        """
+        Parent and Child should be visually separated in the tree view.
+        Therefore we tag them accordingly as 'view' or 'other'
+        @return: void
+        """
+        # Parent
+        if self.hasParent is True:
+            self.type = 'view'
+        # Child
+        else:
+            self.type = 'other'
