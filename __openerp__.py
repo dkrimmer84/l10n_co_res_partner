43c31dc36192fadfe5321aeb28a74541039bcf99
Dominic Krimmer <dominic.krimmer@gmail.com>
2016-06-01 14:58:53 -0500

    adding description for odoo store, including pictures.

--- __openerp__.py
+++ __openerp__.py
@@ -6,19 +6,27 @@
     'license': 'AGPL-3',
     'maintainer': 'dominic.krimmer@gmail.com',
     'website': 'https://www.plastinorte.com',
-    'summary': 'Colombia Terceros: Extended Partner Module - Odoo 9.0',
+    'summary': 'Colombia Terceros: Extended Partner / Contact Module - Odoo 9.0',
+    'images': ['images/main_screenshot.png'],
     'description': """
 Colombia Terceros:
 ======================
 
-    * Redesign of the contact form due to some new rules that have to apply
-    * Additional fields: first and second name, first and second last name
-    * Additional fields: Document Type, Document Number and Types, Tributate regime, CIIU
-    * Automatic Verification of NIT (DV)
-    * Interface to maintain the list of CIIU
-    * Contacts can be found using Identification Number (e.g. NIT)
-    * Country -> State -> Municipality - Logic to avoid confusion of locations with same names
-    * "Identification <-> Document Type" - Combination will be checked as this combination should be unique
+    * Redesign of the contact form due to some local rules that have to apply
+    * Additional fields: first name, second name, last name, second last name
+    * Additional fields: Type of Person, Document Type, Document Number, Tributate regime, CIIU Code
+    * Handling all kind of identification types that are relevant in Colombia
+    * Intelligent form: Fields will be checked for correctness and completeness
+    * Identification Number added into Tree View
+    * Identification Number added into KanBan View
+    * Visual Identifier for incomplete contacts in Kanban- and Tree view
+    * Auto-Complete of NIT: Type in the NIT and the Verification Digit will be calculated automatically
+    * Description of economic activities (CIIU Code), including the entire list to maintain it
+    * Contacts can be found by Identification Number (e.g. NIT)
+    * Added a complete list of all cities and departments in Colombia
+    * Country, Department and Municipality Dependency Logic in order to avoid confusion of locations with the same name
+    * Identification / Document-Type combination will be checked as it should be unique
+    * All changes available in english (en_GB)
 
     """,
     'depends': [
